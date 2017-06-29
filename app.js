47b807c0cef4e47a3a73f7df297fb993e7e4614a
Olivia Burris <oliviaburris11@gmail.com>
2017-06-29 14:43:48 -0400

    up and down buttons work

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = {
     this.flicks = []
     this.max = 0
     this.list = document.querySelector(selectors.listSelector)
+    this.template = document.querySelector(selectors.templateSelector)
 
     document
       .querySelector(selectors.formSelector)
@@ -12,10 +13,63 @@ const app = {
       )
   },
 
-  // takes the flick and makes the list item out of it
+  favFlick(flick, ev) {
+    const listItem = ev.target.closest('.flick')
+    flick.fav = !flick.fav
+
+    if (flick.fav) {
+      listItem.classList.add('fav')
+    } else {
+      listItem.classList.remove('fav')
+    }
+  },
+
+  removeFlick(flick, ev) {
+    // remove from the DOM
+    const listItem = ev.target.closest('.flick')
+    listItem.remove()
+
+    // remove from the array
+    const i = this.flicks.indexOf(flick)
+    this.flicks.splice(i, 1)
+  },
+
   renderListItem(flick) {
-    const item = document.createElement('li')
-    item.textContent = flick.name
+    const item = this.template.cloneNode(true)
+    item.classList.remove('template')
+    item.dataset.id = flick.id
+    item
+      .querySelector('.flick-name')
+      .textContent = flick.name
+
+    item
+      .querySelector('button.remove')
+      .addEventListener(
+        'click', 
+        this.removeFlick.bind(this, flick)
+      )
+
+      item
+        .querySelector('button.up')
+        .addEventListener(
+          'click',
+          this.moveUp.bind(this, flick)
+        )
+
+      item
+        .querySelector('button.down')
+        .addEventListener(
+          'click',
+           this.moveDown.bind(this, flick)
+        )
+
+    item
+      .querySelector('button.fav')
+      .addEventListener(
+        'click', 
+        this.favFlick.bind(this, flick)
+      )
+    
     return item
   },
 
@@ -25,16 +79,32 @@ const app = {
     const flick = {
       id: this.max + 1,
       name: f.flickName.value,
+      fav: false,
     }
 
+    this.flicks.unshift(flick)
+
     const listItem = this.renderListItem(flick)
-    this.list.appendChild(listItem)
+    this.list
+      .insertBefore(listItem, this.list.firstElementChild)
 
     this.max ++
+    f.reset()
   },
+
+ moveUp(flick, ev) {
+        newItem = ev.target.closest('.flick')
+        this.list.insertBefore(newItem, newItem.previousSibling)
+    },
+
+  moveDown(flick, ev) {
+        newItem = ev.target.closest('.flick')
+        this.list.insertBefore(newItem, newItem.nextSibling.nextSibling)
+   },
 }
 
 app.init({
   formSelector: 'form#flick-form',
   listSelector: '#flick-list',
+  templateSelector: '.flick.template',
 })
