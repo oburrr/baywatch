ba84fd28370c7169e4af92ae01be93812eec7803
Olivia Burris <oliviaburris11@gmail.com>
2017-06-29 10:11:26 -0400

    Update to Davey;s progress

--- app.js
+++ app.js
@@ -12,22 +12,10 @@ const app = {
       )
   },
 
+  // takes the flick and makes the list item out of it
   renderListItem(flick) {
     const item = document.createElement('li')
     item.textContent = flick.name
-    favButton = document.createElement('button')
-    favButton.textContent = "Favorite"
-    favButton.setAttribute("class", "favButton")
-
-    item.appendChild(favButton)
-    favButton.addEventListener('click', this.favButtonChanger)
-
-    deleteButton = document.createElement('button')
-    deleteButton.textContent = "Delete"
-    deleteButton.setAttribute("class", "deleteButton")
-    item.appendChild(deleteButton)
-    deleteButton.addEventListener('click', this.favButtonDeleter)
-   item.setAttribute("class", "item")
     return item
   },
 
@@ -38,29 +26,15 @@ const app = {
       id: this.max + 1,
       name: f.flickName.value,
     }
+
     const listItem = this.renderListItem(flick)
     this.list.appendChild(listItem)
-    let i = flick.id - 1
-    this.flicks[i] = flick.name
-    console.log(this.flicks)
 
     this.max ++
   },
-
-  favButtonChanger(ev) {
-    f = ev.target.parentElement
-    f.style.backgroundColor = "#e7d5d5"
-  },
-
-  favButtonDeleter(ev) {
-    f = ev.target.parentElement
-    f.remove(f)
-  },
-
 }
 
 app.init({
-  formSelector:       'form#flick-form',
-  listSelector:       '#flick-list',
-  favButtonSelector:  '.favButton',
+  formSelector: 'form#flick-form',
+  listSelector: '#flick-list',
 })
